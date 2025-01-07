dd840cc44215df9b33035fa0ffe1feed2caae51c
kainowf61 <kn06102003@gmail.com>
2025-01-08 01:52:01 +0700

    Updates: Render component through router.

--- static/src/portal_components/shop_components/shop/shop.js
+++ static/src/portal_components/shop_components/shop/shop.js
@@ -1,10 +1,11 @@
 /** @odoo-module **/
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { BlockBanner } from "../shop_block_banner/block_banner";
 import { FeaturedCategories } from "../shop_featured_categories/featured_categories";
 import { CategoriesBanner } from "../shop_categories/categories_banner";
 import { FlashDeal } from "../shop_flash_deals/flash_deal";
+
 class Shop extends Component {
 
     static template = "enmasys_multiple_vendor.Shop";
@@ -14,10 +15,56 @@ class Shop extends Component {
         CategoriesBanner,
         FlashDeal
     };
-    static props = {};
+
+    // static defaultProps = {
+    //     dev: false,
+    // }
+    // static props = {
+    //     dev: { type: Boolean, value: this.dev},
+    // };
 
     setup() {
+        this.initialView = null;
+        this.checkPath();
+        this.state = useState({
+            currentView: this.initialView,
+        });
+        this.generateComponent();
+    }
 
+    //**
+    //
+    //
+    //
+    get getComponent() {
+        return this.mappedComponents[this.initialView] || Shop;
+    }
+    //**
+    /* This function check current path starts with `/shop/`
+    /* and capture dynamic parameters after it, then render the corresponding component.
+    */
+    checkPath() {
+        const path = window.location.pathname;
+        const match = path.match(/^\/shop\/(.+)/);
+        if (!match) {
+            this.initialView = null;
+        } else {
+            this.initialView = match ? match[1].replace(/-/g, "") : "shop";
+        }
+    }
+
+    //**
+    /* This function mapped from the `components` class property and-
+    /* grant it to this.mappedComponents
+    */
+    generateComponent() {
+        this.mappedComponents = Object.entries(Shop.components).reduce((acc, [key, value]) => {
+            // Convert the key to lowercase
+            const newKey = key.toLowerCase();
+            // Add the key-value pair to the accumulator
+            acc[newKey] = value;
+            return acc;
+        }, {});
     }
 }
 registry.category("public_components").add("enmasys_multiple_vendor.Shop", Shop);
